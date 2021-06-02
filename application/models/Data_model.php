4a329a248a0f9a6fde519eb7eb86dd02af351c4b
xsomad <xsomad79@gmail.com>
2021-06-02 13:10:47 +0700

    Crud Master Jenis Ruangan modul Delete 2th

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -60,17 +60,17 @@ class Data_model extends CI_Model
     foreach($records as $record ){
 
         $data[] = array( 
-           "no"=>$no++,
-           "kode"=>$record->kode,
-           "range_jam"=>$record->range_jam,
+         "no"=>$no++,
+         "kode"=>$record->kode,
+         "range_jam"=>$record->range_jam,
 
 
 
-           "Aksi" => "
-           <a href='javascript:void(0)' class='badge badge-danger item_hapus' data-toggle='Modal' data-placement='bottom' title='Delete' data=$record->kode ;'><span class='far fa-trash-alt'></span></a>
-           <a href='javascript:void(0)' class='badge badge-warning tampilModaleditjam' data-toggle='Modal' data-target='#ModalEdit' data-placement='bottom' title='Edit' data-id=$record->kode  ;'><span class='far fa-edit'></span></a>
-           "
-       ); 
+         "Aksi" => "
+         <a href='javascript:void(0)' class='badge badge-danger item_hapus' data-toggle='Modal' data-placement='bottom' title='Delete' data=$record->kode ;'><span class='far fa-trash-alt'></span></a>
+         <a href='javascript:void(0)' class='badge badge-warning tampilModaleditjam' data-toggle='Modal' data-target='#ModalEdit' data-placement='bottom' title='Edit' data-id=$record->kode  ;'><span class='far fa-edit'></span></a>
+         "
+     ); 
         
     }
 
@@ -171,16 +171,16 @@ public function getHariMaster($postData=null)
     foreach($records as $record ){
 
         $data[] = array( 
-           "no"=>$no++,
-           "kode"=>$record->kode,
-           "nama"=>$record->nama,
+         "no"=>$no++,
+         "kode"=>$record->kode,
+         "nama"=>$record->nama,
 
 
-           "Aksi" => "
-           <a href='javascript:void(0)' class='badge badge-danger item_hapus' data-placement='bottom' title='Delete' data=$record->kode ;'><span class='far fa-trash-alt'></span></a>
-           <a href='javascript:void(0)' class='badge badge-warning edit_hari' data-placement='bottom' title='Edit' data-id=$record->kode  ;'><span class='far fa-edit'></span></a>
-           "
-       ); 
+         "Aksi" => "
+         <a href='javascript:void(0)' class='badge badge-danger item_hapus' data-placement='bottom' title='Delete' data=$record->kode ;'><span class='far fa-trash-alt'></span></a>
+         <a href='javascript:void(0)' class='badge badge-warning edit_hari' data-placement='bottom' title='Edit' data-id=$record->kode  ;'><span class='far fa-edit'></span></a>
+         "
+     ); 
         
     }
 
@@ -281,16 +281,16 @@ public function getTAMaster($postData=null)
     foreach($records as $record ){
 
         $data[] = array( 
-           "no"=>$no++,
-           "kode"=>$record->kode,
-           "tahun"=>$record->tahun,
+         "no"=>$no++,
+         "kode"=>$record->kode,
+         "tahun"=>$record->tahun,
 
 
-           "Aksi" => "
-           <a href='javascript:void(0)' class='badge badge-danger item_hapusta' data-placement='bottom' title='Delete' data=$record->kode ;'><span class='far fa-trash-alt'></span></a>
-           <a href='javascript:void(0)' class='badge badge-warning edit_hari' data-placement='bottom' title='Edit' data-id=$record->kode ;'><span class='far fa-edit'></span></a>
-           "
-       ); 
+         "Aksi" => "
+         <a href='javascript:void(0)' class='badge badge-danger item_hapusta' data-placement='bottom' title='Delete' data=$record->kode ;'><span class='far fa-trash-alt'></span></a>
+         <a href='javascript:void(0)' class='badge badge-warning edit_hari' data-placement='bottom' title='Edit' data-id=$record->kode ;'><span class='far fa-edit'></span></a>
+         "
+     ); 
         
     }
 
@@ -377,46 +377,46 @@ public function getDosenMaster($postData=null)
     $this->db->select('*');
      // $this->db->select("CONCAT(' ', FirstName, LastName) AS Name");
     if($searchQuery != '')
-     $this->db->where($searchQuery);
- $this->db->order_by($columnName, $columnSortOrder);
- $this->db->limit($rowperpage, $start);
- $this->db->select('guru.*', 'status_dosen.kode as kd', 'status_dosen.status');
- $this->db->from('guru');
- $this->db->join('status_dosen', 'guru.status_dosen=status_dosen.kode');
- $records = $this->db->get()->result();
+       $this->db->where($searchQuery);
+   $this->db->order_by($columnName, $columnSortOrder);
+   $this->db->limit($rowperpage, $start);
+   $this->db->select('guru.*', 'status_dosen.kode as kd', 'status_dosen.status');
+   $this->db->from('guru');
+   $this->db->join('status_dosen', 'guru.status_dosen=status_dosen.kode');
+   $records = $this->db->get()->result();
 
- $data = array();
+   $data = array();
 
- $no = 1;
- foreach($records as $record ){
+   $no = 1;
+   foreach($records as $record ){
 
     $data[] = array( 
-       "no"=>$no++,
-       "kode"=>$record->kode,
-       "nip"=>$record->nip,
-       "nama"=>$record->nama,
-       "alamat"=>$record->alamat,
-       "telp"=>$record->telp,
-       "status_dosen"=> $record->status,
+     "no"=>$no++,
+     "kode"=>$record->kode,
+     "nip"=>$record->nip,
+     "nama"=>$record->nama,
+     "alamat"=>$record->alamat,
+     "telp"=>$record->telp,
+     "status_dosen"=> $record->status,
            // "status_dosen"=> if ($record->status_dosen == "1") {"11"} else {"22"
              # code...
            // },
 
            // $record->status_dosen,           
 
-       "Aksi" => "
-       <a href='javascript:void(0)' class='badge badge-danger item_hapusdosen' data-placement='bottom' title='Delete' data=$record->kode data-id=$record->kode ;'><span class='far fa-trash-alt'></span></a>
-       <a href='javascript:void(0)' class='badge badge-warning edit_dosen' data-placement='bottom' title='Edit' data-id=$record->kode ;'><span class='far fa-edit'></span></a>"
-   ); 
+     "Aksi" => "
+     <a href='javascript:void(0)' class='badge badge-danger item_hapusdosen' data-placement='bottom' title='Delete' data=$record->kode data-id=$record->kode ;'><span class='far fa-trash-alt'></span></a>
+     <a href='javascript:void(0)' class='badge badge-warning edit_dosen' data-placement='bottom' title='Edit' data-id=$record->kode ;'><span class='far fa-edit'></span></a>"
+ ); 
 
 }
 
      ## Response 
 $response = array(
-   "draw" => intval($draw),
-   "iTotalRecords" => $totalRecords,
-   "iTotalDisplayRecords" => $totalRecordwithFilter,
-   "aaData" => $data
+ "draw" => intval($draw),
+ "iTotalRecords" => $totalRecords,
+ "iTotalDisplayRecords" => $totalRecordwithFilter,
+ "aaData" => $data
 );
 return $response;
 }
@@ -711,8 +711,6 @@ public function delljenisruangan($idj)
 
 public function saveeditjenisruangan($idj, $saveedit)
 {
-    // $hasil=$this->db->query("UPDATE ruang SET nama='$nama', kapasitas='$kapasitas', id_type='$id_type', lantai='$lantai', id_jenis='$id_jenis', id_ruang='$id_ruang' WHERE kode='$kode'");
-    // return $hasil;
     $this->db->where('idj', $idj);
     $this->db->update('jenis_ruang', $saveedit);
 }
