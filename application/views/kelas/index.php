dc5bb1dcf95daf2d7bc06543adde0014209c98c4
xsomad <xsomad79@gmail.com>
2021-06-19 12:27:43 +0700

    1

--- application/views/kelas/index.php
+++ application/views/kelas/index.php
@@ -145,7 +145,6 @@
                         </div>
 
                     </div>             
-
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
                         <button class="btn_edit btn btn-danger" id="btn_editkelkelas">Save</button>
